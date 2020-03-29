32e8bbce1e42ab6aed52f0ec7d0ee43ebcb7a4ec
ovsienkonikita <ovsienkonikitav@gmail.com>
2020-03-29 23:32:34 +0300

    My Turn: web_crawl is now async

--- days/021-024-quart-async/your_turn/day_2/web_crawl/program.py
+++ days/021-024-quart-async/your_turn/day_2/web_crawl/program.py
@@ -1,40 +1,44 @@
-import requests
+import asyncio
+
 import bs4
+import aiohttp
 from colorama import Fore
 
 
-def get_html(episode_number: int) -> str:
+async def get_html(episode_number: int) -> str:
+    url = f"https://talkpython.fm/{episode_number}"
     print(Fore.YELLOW + f"Getting HTML for episode {episode_number}", flush=True)
-
-    url = f'https://talkpython.fm/{episode_number}'
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    return resp.text
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            return await response.text()
 
 
 def get_title(html: str, episode_number: int) -> str:
     print(Fore.CYAN + f"Getting TITLE for episode {episode_number}", flush=True)
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    header = soup.select_one('h1')
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    header = soup.select_one("h1")
     if not header:
         return "MISSING"
 
     return header.text.strip()
 
 
-def main():
-    get_title_range()
+async def main():
+    await get_title_range()
     print("Done.")
 
 
-def get_title_range():
+async def get_title_range():
     # Please keep this range pretty small to not DDoS my site. ;)
-    for n in range(150, 170):
-        html = get_html(n)
-        title = get_title(html, n)
-        print(Fore.WHITE + f"Title found: {title}", flush=True)
+    await asyncio.gather(*[get_title_by_number(n) for n in range(150, 170)])
 
 
-if __name__ == '__main__':
-    main()
+async def get_title_by_number(n: int):
+    html = await get_html(n)
+    title = get_title(html, n)
+    print(Fore.WHITE + f"Title found: {title}", flush=True)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
