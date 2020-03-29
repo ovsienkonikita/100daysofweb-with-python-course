ed347c0b20c860942ed38ebd6c8d71c7cae07c99
ovsienkonikita <ovsienkonikitav@gmail.com>
2020-03-29 22:10:32 +0300

    My turn: Displaying "Base Happiness" and "Capture Rate" in pokemons table

--- days/017-020-flask-call-apis/code/demo.py
+++ days/017-020-flask-call-apis/code/demo.py
@@ -1,4 +1,4 @@
 from program import app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(load_dotenv=False)
--- days/017-020-flask-call-apis/code/program/routes.py
+++ days/017-020-flask-call-apis/code/program/routes.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
 
 import requests
 from flask import render_template, request
@@ -8,46 +10,43 @@ from program import app
 time_now = str(datetime.today())
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html', title='Template Demo', time=time_now)
+    return render_template("index.html", title="Template Demo", time=time_now)
 
 
-@app.route('/100Days')
+@app.route("/100Days")
 def p100days():
-    return render_template('100Days.html')
+    return render_template("100Days.html")
 
 
-@app.route('/chuck')
+@app.route("/chuck")
 def chuck():
     joke = get_chuck_joke()
-    return render_template('chuck.html',
-                           joke=joke)
+    return render_template("chuck.html", joke=joke)
 
 
-@app.route('/pokemon', methods=['GET', 'POST'])
+@app.route("/pokemon", methods=["GET", "POST"])
 def pokemon():
-    pokemon = []
-    if request.method == 'POST' and 'pokecolour' in request.form:
-        colour = request.form.get('pokecolour')
-        pokemon = get_poke_colours(colour)
-    return render_template('pokemon.html',
-                           pokemon=pokemon)
+    pokemons = []
+    if request.method == "POST" and "pokecolour" in request.form:
+        colour = request.form.get("pokecolour")
+        pokemons = get_pokemons_by_color(colour)
+    return render_template("pokemon.html", pokemons=pokemons)
 
 
 def get_chuck_joke():
-    r = requests.get('https://api.chucknorris.io/jokes/random')
+    r = requests.get("https://api.chucknorris.io/jokes/random")
     data = r.json()
-    return data['value']
+    return data["value"]
 
 
-def get_poke_colours(colour):
-    r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + colour.lower())
+def get_pokemons_by_color(colour):
+    r = requests.get("https://pokeapi.co/api/v2/pokemon-color/" + colour.lower())
     pokedata = r.json()
-    pokemon = []
 
-    for i in pokedata['pokemon_species']:
-        pokemon.append(i['name'])
+    with ThreadPoolExecutor(max_workers=cpu_count() * 2) as executor:
+        pokemons = executor.map(requests.get, (p["url"] for p in pokedata["pokemon_species"]))
 
-    return pokemon
+    return (pokemon.json() for pokemon in pokemons)
